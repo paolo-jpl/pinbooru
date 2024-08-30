5e46c52a8e9aa9b3973b70328ae3f2ae2b55685f
ludicorgi <jpaolo.lojo@gmail.com>
2024-08-30 14:27:57 +0800

    add conditional rendering on sidebar

--- src/ImgCollection.tsx
+++ src/ImgCollection.tsx
@@ -1,6 +1,3 @@
-import Sidebar from "./minor_components/Sidebar";
-import Navbar from "./minor_components/Navbar";
-
 export default function ImgCollection({user}: {user: string}){
   return(
     <main >
--- src/minor_components/Sidebar.tsx
+++ src/minor_components/Sidebar.tsx
@@ -1,8 +1,50 @@
+import { useLocation } from "react-router-dom";
+
 export default function Sidebar(){
+  const location = useLocation();
+
   return(
     <div className="sidebar">
       <input type="text"/>
-      tags
+
+      {/* match path /post/:id */}
+      { /^\/post\/(.+)/.test(location.pathname) ?
+        imageTags() : defaultTags()
+      }
+
     </div>
+  )
+}
+
+function defaultTags(){
+  return(
+    <>
+      <ul className="sidebar-list">
+        <div className="sidebar-header">Tags</div>
+        <li>one</li>
+        <li>one</li>
+        <li>one</li>
+      </ul>
+    </>
+  )
+}
+
+function imageTags(){
+  return(
+    <>
+      <ul className="sidebar-list">
+        <div className="tag-category">Artist</div>
+        <li>one</li>
+        <li>one</li>
+        <li>one</li>
+      </ul>
+
+      <ul className="sidebar-list">
+        <div className="sidebar-header">Information</div>
+        <li>Uploader:</li>
+        <li>Date: </li>
+        <li>Source: </li>
+      </ul>
+    </>
   )
 }
