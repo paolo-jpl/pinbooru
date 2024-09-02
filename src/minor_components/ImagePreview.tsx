3feb002cfe2dccc9f5b426c29121343945f44880
ludicorgi <jpaolo.lojo@gmail.com>
2024-09-02 18:07:18 +0800

    added lazy image loading

--- src/minor_components/ImagePreview.tsx
+++ src/minor_components/ImagePreview.tsx
@@ -1,7 +1,7 @@
 export default function ImagePreview({image} : any){
   return(
     <div style={{width: "250px", height: "250px"}}>
-        <img src={image.imgURL}></img>
+        <img src={image.imgURL} loading="lazy"></img>
     </div>
   )
 }
