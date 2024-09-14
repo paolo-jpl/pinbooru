934d11644e14389f72e8dd0962abb3d43e19a796
ludicorgi <jpaolo.lojo@gmail.com>
2024-09-15 02:32:18 +0800

    add util functions file

---
+++ src/util/util.ts
@@ -0,0 +1,9 @@
+export function arrangeTags(){
+  return [];
+}
+
+export function formatDate(timestamp: string){
+  let date = new Date(timestamp);
+  let formattedDate = `${(date.getMonth() + 1)}/${date.getDate()}/${date.getFullYear()}`;
+  return formattedDate;
+}
